5ae92ad102116be327b18774b839d5b3de76a2a6
Michael Kennedy <michael@talkpython.fm>
2024-11-07 13:35:44 -0800

    convert to pyproject file

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import io
 import os
 import re
 
-from setuptools import find_packages
 from setuptools import setup
 
 
@@ -13,24 +12,11 @@ def read(filename):
         return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
 
 
-def read_version():
-    filename = os.path.join(os.path.dirname(__file__), 'chameleon_flask', '__init__.py')
-    with open(filename, mode='r', encoding='utf-8') as fin:
-        for line in fin:
-            if line and line.strip() and line.startswith('__version__'):
-                return line.split('=')[1].strip().strip("'").strip('"')
-
-    return '0.0.0.0'
-
-
-requirements_txt = os.path.join(os.path.dirname(__file__), 'requirements.txt')
-
-with open(requirements_txt, 'r', encoding='utf-8') as fin:
-    requires = [line.strip() for line in fin if line and line.strip() and not line.strip().startswith('#')]
+requires = ['flask[async]', 'Chameleon']
 
 setup(
     name='chameleon_flask',
-    version=read_version(),
+    version='0.0.1',
     url='https://github.com/mikeckennedy/chameleon-flask',
     license='MIT',
     author='Michael Kennedy',
@@ -38,15 +24,7 @@ setup(
     description='Adds integration of the Chameleon template language to Flask and Quart.',
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
-    packages=find_packages(
-        exclude=(
-            'tests',
-            'example',
-            'readme_resources',
-            'build',
-            'dist',
-        )
-    ),
+    packages=['chameleon_flask'],
     install_requires=requires,
     classifiers=[
         'Development Status :: 3 - Alpha',
