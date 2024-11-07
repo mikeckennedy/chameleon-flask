612e21ef5654e0c52e688905ba422737f2d344f0
Michael Kennedy <michael@talkpython.fm>
2024-11-07 13:19:40 -0800

    and now the tests work as well.

--- tests/test_not_found.py
+++ tests/test_not_found.py
@@ -1,6 +1,7 @@
 import asyncio
 
 import flask
+
 import flask_chameleon
 import flask_chameleon as fc
 
@@ -14,9 +15,10 @@ def test_friendly_404_sync_method(setup_global_template):
         return {'a': a, 'b': b, 'c': c}
 
     resp = view_method(1, 2, 3)
+    html = response_to_html(resp)
     assert isinstance(resp, flask.Response)
     assert resp.status_code == 404
-    assert b'<h1>This is a pretty 404 page.</h1>' in resp.body
+    assert '<h1>This is a pretty 404 page.</h1>' in html
 
 
 # setup_global_template - needed as pytest mix-in.
@@ -28,9 +30,10 @@ def test_friendly_404_custom_template_sync_method(setup_global_template):
         return {'a': a, 'b': b, 'c': c}
 
     resp = view_method(1, 2, 3)
+    html = response_to_html(resp)
     assert isinstance(resp, flask.Response)
     assert resp.status_code == 404
-    assert b'<h1>Another pretty 404 page.</h1>' in resp.body
+    assert '<h1>Another pretty 404 page.</h1>' in html
 
 
 # setup_global_template - needed as pytest mix-in.
@@ -42,6 +45,12 @@ def test_friendly_404_async_method(setup_global_template):
         return {'a': a, 'b': b, 'c': c}
 
     resp = asyncio.run(view_method(1, 2, 3))
+    html = response_to_html(resp)
     assert isinstance(resp, flask.Response)
     assert resp.status_code == 404
-    assert b'<h1>This is a pretty 404 page.</h1>' in resp.body
+    assert '<h1>This is a pretty 404 page.</h1>' in html
+
+
+def response_to_html(response: flask.Response) -> str:
+    # noinspection PyUnresolvedReferences
+    return response.response[0].decode('utf-8')
--- tests/test_render.py
+++ tests/test_render.py
@@ -1,9 +1,10 @@
 import asyncio
 
 import flask
-import flask_chameleon as fc
 # noinspection PyPackageRequirements
 import pytest
+
+import flask_chameleon as fc
 
 
 # setup_global_template - needed as pytest mix-in.
@@ -40,7 +41,7 @@ def test_default_template_name_pt(setup_global_template):
     resp = index(1, 2, 3)
     assert isinstance(resp, flask.Response)
     assert resp.status_code == 200
-    html = resp.body.decode('utf-8')
+    html = response_to_html(resp)
     assert '<h1>Hello default WORLD!</h1>' in html
 
 
@@ -54,7 +55,7 @@ def test_default_template_name_no_parentheses(setup_global_template):
     resp = index(1, 2, 3)
     assert isinstance(resp, flask.Response)
     assert resp.status_code == 200
-    html = resp.body.decode('utf-8')
+    html = response_to_html(resp)
     assert '<h1>Hello default WORLD!</h1>' in html
 
 
@@ -66,7 +67,7 @@ def test_default_template_name_html(setup_global_template):
     resp = details(1, 2, 3)
     assert isinstance(resp, flask.Response)
     assert resp.status_code == 200
-    html = resp.body.decode('utf-8')
+    html = response_to_html(resp)
     assert '<h1>Hello default WORLD!</h1>' in html
 
 
@@ -95,9 +96,15 @@ def test_can_decorate_dict_async_method(setup_global_template):
 def test_direct_response_pass_through():
     @fc.template('home/index.pt')
     def view_method(a, b, c):
-        return flask.Response(content='abc', status_code=418)
+        return flask.Response(response='abc', status=418)
 
     resp = view_method(1, 2, 3)
+    html = response_to_html(resp)
     assert isinstance(resp, flask.Response)
     assert resp.status_code == 418
-    assert resp.body == b'abc'
+    assert html == 'abc'
+
+
+def response_to_html(response: flask.Response) -> str:
+    # noinspection PyUnresolvedReferences
+    return response.response[0].decode('utf-8')
